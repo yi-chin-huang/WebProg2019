cbe99555a217ea479189b852d44b3993b826d257
黃意芹 <ctps930080@gmail.com>
2019-04-01 18:22:34 +0800

    prac4 coment

--- Practice04/own/src/components/item.js
+++ Practice04/own/src/components/item.js
@@ -4,29 +4,29 @@ class Item extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {status: "active"};
+        this.state = {isCompleted: "active"};
         // this.status = "active";
     }
     
-    changeStatus = () => {
-        // let text = this.getElementsByTagName('H1')[0];
-        let text = this.refs.itemContent;
-        console.log(this.innerHTML);
-        if(this.state.status == "active") 
-        {
-            this.setState(() => ({status: "completed"}));
-            text.style["textDecoration"] = "line-through";
-            text.style["opacity"] = 0.5;
+//     changeStatus = () => {
+//         // let text = this.getElementsByTagName('H1')[0];
+//         let text = this.refs.itemContent;
+//         console.log(this.innerHTML);
+//         if(this.state.status == "active") 
+//         {
+//             this.setState(() => ({status: "completed"}));
+//             text.style["textDecoration"] = "line-through";
+//             text.style["opacity"] = 0.5;
 
-        }
-        else
-        {
-            this.setState(() => ({status: "active"}));
-            text.style["textDecoration"] = "none";
-            text.style["opacity"] = 1;
+//         }
+//         else
+//         {
+//             this.setState(() => ({status: "active"}));
+//             text.style["textDecoration"] = "none";
+//             text.style["opacity"] = 1;
 
-        }
-   }
+//         }
+//    }
 
 
     render() {
@@ -34,10 +34,10 @@ class Item extends React.Component {
         return (
             <li class = "todo-app__item">
                 <div class = "todo-app__checkbox">
-                    <input id = {this.props.id} type = "checkbox" onClick = {this.props.check}></input>
+                    <input id = {this.props.id} type = "checkbox" checked = {this.props.checked} onClick = {this.props.check}></input>
                     <label for = {this.props.id}></label>
                 </div>
-                <h1 class = "todo-app__item-detail" ref = "itemContent">{this.props.content}</h1>
+                <h1 class = "todo-app__item-detail" ref = "itemContent" style = {this.props.style} >{this.props.content}</h1>
                 <img src = {x} class = "todo-app__item-x" onClick = {this.props.del} id = {this.props.id}/>
             </li>      
         );
---
+++ Practice04/review/comment.md
@@ -0,0 +1,7 @@
+## Practice04 comment
+1. 完程度：20%，只有做出新增功能，刪除功能有嘗試做但沒有顯示出來，footer按鈕皆無效
+2. coding quality：網頁各部分有切開成不同檔，縮排整齊
+4. 正確性：刪除只改class attribute不會即時在頁面上更新，所以看不出刪除，建議改為react component並使用setState更新刪除狀況
+5. 值得學習的地方：檔案切割乾淨，code整齊
+7. 建議改進的地方：建議item可改為react component
+8. 其他心得：加油！
